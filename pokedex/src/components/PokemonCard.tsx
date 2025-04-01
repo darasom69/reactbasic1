ea13255fd874a90b1015f345a06ce1d59144c42e
darasom69 <daha69@hotmail.fr>
2025-04-01 16:17:25 +0200

    quete3

--- pokedex/src/components/PokemonCard.tsx
+++ pokedex/src/components/PokemonCard.tsx
@@ -1,10 +1,22 @@
-function PokemonCard() {
+  const pokemonList = [
+    {
+    name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+    function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
-      <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption> Bulbasaur </figcaption>
-      </figure>
+      <section>
+        <h2>{pokemon.name}</h2>
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      </section>
     );
   }
-
+  
   export default PokemonCard;
